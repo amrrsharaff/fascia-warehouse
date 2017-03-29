ce8c05ef89b2cfda1510f414adc93420bf0d8e46
Omar Sherif <omar.sherif@mail.utoronto.ca>
2017-03-29 18:42:48 -0400

    fixed design aspects in Loader.java

--- project/src/Loader.java
+++ project/src/Loader.java
@@ -8,8 +8,12 @@ public class Loader {
   
   /** The logger used to log events. */
   private static final Logger logger = Logger.getLogger(Reader.class.getName());
-
+  /** The name of the Loader. */
   private String name;
+  /** The order to be loaded */
+  private Order toBeLoaded;
+  /** The skus to be scanned/rescanned */
+  private ArrayList<String> rescannedSKUs;
   
   public String getName() {
     return name;
@@ -19,10 +23,7 @@ public class Loader {
 //    this.name = name;
 //  }
 
-  /** The order to be loaded */
-  private Order toBeLoaded;
-  /** The skus to be scanned/rescanned */
-  private ArrayList<String> rescannedSKUs;
+
     /**
      * Initializes a loader.
      */
